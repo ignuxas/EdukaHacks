63bbd2824500c0331eb9f8985d14534cbfe0ffa2
Ignas Mikolaitis <80627100+ignuxas@users.noreply.github.com>
2021-12-01 08:14:16 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
 //By: Ignas Mikolaitis
+//Thanks to: GTDT (Optimizations)
 //Website: ignuxas.com
 
 window.open("https://klase.eduka.lt/api/student/quiz-templates/result-xml/"+(window.location.pathname).slice(30, 38)+"/perziura")
